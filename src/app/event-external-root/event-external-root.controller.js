b7c0641af43308725fe622c93e83fdffeee4ba67
Asma AYARI <aayari@linagora.com>
2020-10-06 15:36:00 +0100

    #20 resolve action links in excal app and update url action

--- src/app/event-external-root/event-external-root.controller.js
+++ src/app/event-external-root/event-external-root.controller.js
@@ -3,14 +3,33 @@
 angular.module('esn.calendar.event-consultation')
   .controller('CalEventExternalRootController', CalEventExternalRootController);
 
-function CalEventExternalRootController($scope, result) {
+function CalEventExternalRootController($rootScope, $location, result, calEventExternalRootService) {
   const self = this;
+
+  $rootScope.$on('UPDATE_ACTION_EXCAL', function(event, newLink) {
+    const newJWT = parseJwt(newLink);
+
+    if (newJWT) {
+      $location.search('jwt', newJWT);
+    }
+  });
 
   if (result.error) {
     self.error = result.error;
   } else {
     self.eventJcal = result.eventJSON;
     self.attendeeEmail = result.attendeeEmail;
-    self.links = result.links;
+    self.links = {};
+    Object.keys(result.links).forEach(action => {
+      const jwt = parseJwt(result.links[action]);
+
+      self.links[action] = calEventExternalRootService.getChangeParticipationUrl(jwt);
+    });
+  }
+
+  function parseJwt(link) {
+    const token = link.match(/jwt=(.*)/);
+
+    return token ? token[1] : null;
   }
 }
